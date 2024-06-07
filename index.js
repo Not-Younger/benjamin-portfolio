08f4d08a6a2a1df3b4d52615666640792601b7ca
Jonathan Young <jonathanyoung7718@gmail.com>
2024-06-06 21:30:47 -0500

    hero structure done

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const circle = document.getElementById('circle');
+let mouseX = 0, mouseY = 0, posX = 0, posY = 0;
 
 document.addEventListener('mousemove', (e) => {
   const height = circle.offsetHeight;
@@ -11,7 +12,7 @@ document.addEventListener('mousemove', (e) => {
   }
 
   setTimeout(() => { 
-    circle.style.left = `${e.pageX - width/2}px`;
-    circle.style.top = `${e.pageY - height/2}px`;
+    circle.style.left = `${e.clientX - width/2}px`;
+    circle.style.top = `${e.clientY - height/2}px`;
   }, 20);
 });
