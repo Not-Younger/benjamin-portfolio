7ada5f28856d80ff8b4cf8a8a1c5b924d0f5de89
Jonathan Young <jonathanyoung7718@gmail.com>
2024-06-06 21:03:29 -0500

    added cicle on cursor

---
+++ index.js
@@ -0,0 +1,17 @@
+const circle = document.getElementById('circle');
+
+document.addEventListener('mousemove', (e) => {
+  const height = circle.offsetHeight;
+  const width = circle.offsetWidth;
+
+  if (e.target.tagName === 'A') {
+    circle.classList.add('big');
+  } else {
+    circle.classList.remove('big');
+  }
+
+  setTimeout(() => { 
+    circle.style.left = `${e.pageX - width/2}px`;
+    circle.style.top = `${e.pageY - height/2}px`;
+  }, 20);
+});
