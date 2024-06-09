5e9c9a2e6c615c4ff90190094daca189336ec1e5
Jonathan Young <jonathanyoung7718@gmail.com>
2024-06-09 12:41:15 -0500

    animated button

--- index.js
+++ index.js
@@ -45,3 +45,31 @@ changeText();
 
 const year = new Date().getFullYear();
 document.getElementById('year').innerHTML = year;
+
+const menu = document.getElementById('menu-btn');
+const icon = document.getElementById('icon');
+const bar1 = document.getElementsByClassName('bar1')[0];
+const bar2 = document.getElementsByClassName('bar2')[0];
+const bar3 = document.getElementsByClassName('bar3')[0];
+menu.addEventListener('click', () => {
+  if (menu.classList.contains('open')) {
+    icon.classList.remove('io');
+    icon.classList.add('ic');
+    bar1.classList.remove('o1');
+    bar1.classList.add('o1c');
+    bar2.classList.remove('o2');
+    bar2.classList.add('o2c');
+    bar3.classList.remove('o3');
+    bar3.classList.add('o3c');
+  } else {
+    icon.classList.remove('ic');
+    icon.classList.add('io');
+    bar1.classList.remove('o1c');
+    bar1.classList.toggle('o1');
+    bar2.classList.remove('o2c');
+    bar2.classList.toggle('o2');
+    bar3.classList.remove('o3c');
+    bar3.classList.toggle('o3');
+  }
+  menu.classList.toggle('open');
+})
