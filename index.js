c0ed9b8074ad9fcec886a9b6585e57b5af4b21a5
Jonathan Young <jonathanyoung7718@gmail.com>
2024-07-07 16:15:44 -0500

    holy smokes

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+// Circle Cursor
 const circle = document.getElementById('circle');
 let mouseX = 0, mouseY = 0, posX = 0, posY = 0;
 
+function isTouchScreenOnlyDevice() {
+  return window.matchMedia("(hover: none)").matches && window.matchMedia("(pointer: coarse)").matches;
+}
+
 document.addEventListener('mousemove', (e) => {
+
+  if (isTouchScreenOnlyDevice()) return;
+  
   const height = circle.offsetHeight;
   const width = circle.offsetWidth;
 
@@ -17,68 +25,96 @@ document.addEventListener('mousemove', (e) => {
   }, 20);
 });
 
-
+// H1 Text Animation
 const text = document.getElementById('typed');
 const cursor = document.getElementsByClassName('typed-cursor')[0];
-const skills = ['Photographer', 'Videographer', 'Marketer', 'Drone Pilot'];
+const skills = ['Photographer', 'Videographer', 'Marketer', 'Drone-Pilot'];
 async function changeText() {
   var i = 0;
   while (i < 100) {
+    var current = '';
     text.innerHTML = '';
     var index = i % skills.length;
     for (var j = 0; j < skills[index].length; j++) {
-      text.innerHTML += skills[index][j];
+      current += `<span>${skills[index][j]}</span>`;
+      text.innerHTML = current + cursor.outerHTML;
       await new Promise(r => setTimeout(r, 100));
     }
+    const cursorBlink = cursor;
+    cursorBlink.classList.add('blink');
+    text.innerHTML = current + cursorBlink.outerHTML;
     cursor.classList.add('blink');
     await new Promise(r => setTimeout(r, 2000));
-    cursor.classList.remove('blink');
     for (var j = skills[index].length-1; j >= 0; j--) {
-      text.innerHTML = skills[index].slice(0, j);
+      current = current.slice(0, -14);
+      text.innerHTML = current + cursor.outerHTML;
       await new Promise(r => setTimeout(r, 70));
     }
     i++;
   }
 }
+window.onload = () => {
+  if (window.location.pathname === '/index.html') 
+    changeText();
+}
 
-changeText();
-
+// Current Year
 const year = new Date().getFullYear();
 document.getElementById('year').innerHTML = year;
 
-const links = document.getElementById('menu');
-const menu = document.getElementById('menu-btn');
+// Mobile Nav Button
+const navButton = document.getElementById('menu-btn');
+const nav = document.getElementById('mobile-nav');
 const icon = document.getElementById('icon');
+
 const bar1 = document.getElementsByClassName('bar1')[0];
 const bar2 = document.getElementsByClassName('bar2')[0];
 const bar3 = document.getElementsByClassName('bar3')[0];
-menu.addEventListener('click', () => {
-  if (menu.classList.contains('open')) {
-    icon.classList.remove('io');
-    icon.classList.add('ic');
-    bar1.classList.remove('o1');
-    bar1.classList.add('o1c');
-    bar2.classList.remove('o2');
-    bar2.classList.add('o2c');
-    bar3.classList.remove('o3');
-    bar3.classList.add('o3c');
+const barx1 = document.getElementsByClassName('barx1')[0];
+const barx2 = document.getElementsByClassName('barx2')[0];
 
-    links.style.display = 'none';
-    links.classList.remove('mo');
-    links.classList.add('mc');
+navButton.addEventListener('click', () => {
+  if (nav.style.display === 'none') {
+    nav.style.display = 'flex';
+    nav.classList.remove('mobile-nav-close');
+    nav.classList.add('mobile-nav-open');
   } else {
-    icon.classList.remove('ic');
-    icon.classList.add('io');
-    bar1.classList.remove('o1c');
-    bar1.classList.toggle('o1');
-    bar2.classList.remove('o2c');
-    bar2.classList.toggle('o2');
-    bar3.classList.remove('o3c');
-    bar3.classList.toggle('o3');
-
-    links.style.display = 'flex';
-    links.classList.remove('mc');
-    links.classList.add('mo');
+    nav.style.display = 'none';
+    nav.classList.remove('mobile-nav-open');
+    nav.classList.add('mobile-nav-close');
   }
-  menu.classList.toggle('open');
-})
+  
+  if (barx1.style.display === 'none') {
+    bar1.style.display = 'none';
+    bar2.style.display = 'none';
+    bar3.style.display = 'none';
+    barx1.style.display = 'block';
+    barx2.style.display = 'block';
+  } else {
+    bar1.style.display = 'block';
+    bar2.style.display = 'block';
+    bar3.style.display = 'block';
+    barx1.style.display = 'none';
+    barx2.style.display = 'none';
+  }
+
+  icon.animate([
+    { transform: 'rotate(180deg)' },
+  ], {
+    duration: 200,
+    iterations: 1
+  });
+});
+
+// Close Mobile Nav if Screen is Resized
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 1000) {
+    nav.style.display = 'none';
+    bar1.classList.remove('bar1-open');
+    bar1.classList.add('bar1');
+    bar2.classList.remove('bar2-open');
+    bar2.classList.add('bar2');
+    bar3.classList.remove('bar3-open');
+    bar3.classList.add('bar3');
+  }
+});
